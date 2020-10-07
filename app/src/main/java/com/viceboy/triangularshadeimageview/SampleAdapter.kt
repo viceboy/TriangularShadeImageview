2557552ad809bb7c71fba18868675a9b3737fa5b
viceboy <viceboystudios@gmail.com>
2020-10-07 15:08:32 +0530

    Updated gradle files

--- app/src/main/java/com/viceboy/triangularshadeimageview/SampleAdapter.kt
+++ app/src/main/java/com/viceboy/triangularshadeimageview/SampleAdapter.kt
@@ -5,7 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
-import com.viceboy.library.TriangularShadeImageView
+import com.viceboy.widget.TriangularShadeImageView
 
 class SampleAdapter(
     private val listOfItems: MutableList<SampleModel>,
